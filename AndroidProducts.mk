df732b8a18eb7ca0d553bec14d9d2ba4076f8fef
Sachin752002 <103373100+Sachin752002@users.noreply.github.com>
2022-11-04 11:43:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_X01AD.mk
+    $(LOCAL_DIR)/lineage_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_X01AD-user \
-    corvus_X01AD-userdebug \
-    corvus_X01AD-eng
+    lineage_X01AD-user \
+    lineage_X01AD-userdebug \
+    lineage_X01AD-eng
